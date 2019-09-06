a6ecba54f2b1d8f2586054deab51e98f07c07517
Scott Clampet <scottclampet@Scotts-MacBook-Pro-4.local>
2019-09-06 15:36:15 -0600

    create static list of accounts

--- I Have More Money Than/I Have More Money Than/Controllers/HomeViewController.swift
+++ I Have More Money Than/I Have More Money Than/Controllers/HomeViewController.swift
@@ -11,6 +11,19 @@ import UIKit
 class HomeViewController: UICollectionViewController {
     
     private let cellId = "cellId"
+    let accounts: [Account] = {
+        return [
+            Account(id: 1, name: "Royal Bank of Canada", imageName: "bankCanada", amount: 234123, ROI: 12),
+            Account(id: 2, name: "Cayman Islands", imageName: "bankCanada", amount: 5432, ROI: 12),
+            Account(id: 3, name: "Berkshire", imageName: "berkshire", amount: 43454345, ROI: -12),
+            Account(id: 4, name: "Real Estate", imageName: "realEstate", amount: 234123, ROI: 12),
+            Account(id: 5, name: "Gold Bars", imageName: "goldBars", amount: 234122342343, ROI: -12),
+            Account(id: 6, name: "Venture Investments", imageName: "venture", amount: 123, ROI: 12),
+            Account(id: 7, name: "Stuffed Under Mattress", imageName: "mattressMoney", amount: 234123, ROI: 12),
+            Account(id: 8, name: "Mining", imageName: "mining", amount: 2343, ROI: 12),
+            Account(id: 9, name: "Antiquities", imageName: "antiquity", amount: 234123, ROI: -12),
+        ]
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
