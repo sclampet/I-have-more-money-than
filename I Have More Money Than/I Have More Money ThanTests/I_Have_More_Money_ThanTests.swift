1566fc3a334b7dfa552a8e29acd47086e681cd98
Scott Clampet <scottclampet@Scotts-MacBook-Pro-4.local>
2019-09-06 14:37:48 -0600

    initial single view app project creation

---
+++ I Have More Money Than/I Have More Money ThanTests/I_Have_More_Money_ThanTests.swift
@@ -0,0 +1,34 @@
+//
+//  I_Have_More_Money_ThanTests.swift
+//  I Have More Money ThanTests
+//
+//  Created by Scott Clampet on 9/6/19.
+//  Copyright © 2019 Scott Clampet. All rights reserved.
+//
+
+import XCTest
+@testable import I_Have_More_Money_Than
+
+class I_Have_More_Money_ThanTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
