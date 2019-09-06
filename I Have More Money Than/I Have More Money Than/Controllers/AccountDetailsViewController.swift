654f9304d52b693c19661c349f747b5049abd206
Scott Clampet <scottclampet@Scotts-MacBook-Pro-4.local>
2019-09-06 17:19:17 -0600

    create accountdetailscontroller

---
+++ I Have More Money Than/I Have More Money Than/Controllers/AccountDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AccountDetailsViewController.swift
+//  I Have More Money Than
+//
+//  Created by Scott Clampet on 9/6/19.
+//  Copyright © 2019 Scott Clampet. All rights reserved.
+//
+
+import UIKit
+
+class AccountDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .cyan
+    }
+
+}
