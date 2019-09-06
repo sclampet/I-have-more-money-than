5f93501bc8b3f3e0d183d25e4dc270b9b011c137
Scott Clampet <scottclampet@Scotts-MacBook-Pro-4.local>
2019-09-06 15:57:38 -0600

    add account info to textView

--- I Have More Money Than/I Have More Money Than/Views/Cells/AccountCell.swift
+++ I Have More Money Than/I Have More Money Than/Views/Cells/AccountCell.swift
@@ -17,6 +17,19 @@ class AccountCell: BaseCell {
             }
             
             imageView.image = UIImage(named: account.imageName)
+            
+            let color = UIColor(white: 1, alpha: 1)
+            let attributedText = NSMutableAttributedString(string: account.name, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 24, weight: .medium), NSAttributedString.Key.foregroundColor: color])
+            
+            attributedText.append(NSAttributedString(string: "\n\n\(account.amount)", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18), NSAttributedString.Key.foregroundColor: color]))
+            
+//            let paragraphStyle = NSMutableParagraphStyle()
+//            paragraphStyle.alignment = .center
+//
+//            let length = attributedText.string.count
+//            attributedText.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSRange(location: 0, length: length))
+            
+            textView.attributedText = attributedText
         }
     }
     
@@ -27,11 +40,23 @@ class AccountCell: BaseCell {
         return iv
     }()
     
+    let textView: UITextView = {
+        let tv = UITextView()
+        tv.text = ""
+        tv.isEditable = false
+        tv.backgroundColor = .gray
+        return tv
+    }()
+    
     override func setupViews() {
         backgroundColor = .cyan
         addSubview(imageView)
+        addSubview(textView)
         
         addConstraintsWithFormat(format: "V:|[v0]|", views: imageView)
         addConstraintsWithFormat(format: "H:|[v0]|", views: imageView)
+        
+        addConstraintsWithFormat(format: "V:[v0(100)]|", views: textView)
+        addConstraintsWithFormat(format: "H:|[v0]|", views: textView)
     }
 }
