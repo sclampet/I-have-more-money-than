6f057d8105e6f53a8bfa440c9b2d441a6b875f21
Scott Clampet <scottclampet@Scotts-MacBook-Pro-4.local>
2019-09-06 15:31:17 -0600

    create account model

---
+++ I Have More Money Than/I Have More Money Than/Models/Account.swift
@@ -0,0 +1,17 @@
+//
+//  Account.swift
+//  I Have More Money Than
+//
+//  Created by Scott Clampet on 9/6/19.
+//  Copyright © 2019 Scott Clampet. All rights reserved.
+//
+
+import Foundation
+
+struct Account {
+    let id: Int
+    let name: String
+    let imageName: String
+    let amount: Int
+    let ROI: Int
+}
