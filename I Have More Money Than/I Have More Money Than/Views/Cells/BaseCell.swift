5a2c8fe0451f4eb177680a186706163d2525286f
Scott Clampet <scottclampet@Scotts-MacBook-Pro-4.local>
2019-09-06 15:09:05 -0600

    create base collectionview cell

---
+++ I Have More Money Than/I Have More Money Than/Views/Cells/BaseCell.swift
@@ -0,0 +1,24 @@
+//
+//  BaseCell.swift
+//  I Have More Money Than
+//
+//  Created by Scott Clampet on 9/6/19.
+//  Copyright © 2019 Scott Clampet. All rights reserved.
+//
+
+import UIKit
+
+//Template CollectionViewCell to allow for easier setup of new cells
+class BaseCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    private func setupViews() {}
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
