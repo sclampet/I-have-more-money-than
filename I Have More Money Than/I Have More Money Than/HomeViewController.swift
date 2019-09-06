8af42aada441b66d56d44df6a78bb8d48e84457c
Scott Clampet <scottclampet@Scotts-MacBook-Pro-4.local>
2019-09-06 15:05:50 -0600

    conform homeviewcontroller to collectionviewdelegate and datasource methods and display 6 dummy cells

--- I Have More Money Than/I Have More Money Than/HomeViewController.swift
+++ I Have More Money Than/I Have More Money Than/HomeViewController.swift
@@ -9,9 +9,29 @@
 import UIKit
 
 class HomeViewController: UICollectionViewController {
+    
+    private let cellId = "cellId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    }
+}
+
+extension HomeViewController {
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .yellow
+        return cell
     }
 }
 
