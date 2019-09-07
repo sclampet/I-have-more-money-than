e5eacf91f876b90d401c7807c81721a21026e629
Scott Clampet <scottclampet@Scotts-MacBook-Pro-4.local>
2019-09-07 08:41:25 -0600

    add subtitleLabel to header

--- I Have More Money Than/I Have More Money Than/Views/HeaderView.swift
+++ I Have More Money Than/I Have More Money Than/Views/HeaderView.swift
@@ -12,8 +12,18 @@ class HeaderView: UICollectionReusableView {
     
     let titleLabel: UILabel = {
         let label = UILabel()
-        label.text = "Accounts that make less than you do"
+        label.text = "Accounts"
         label.font = UIFont.boldSystemFont(ofSize: 32)
+        label.numberOfLines = 2
+        label.lineBreakMode = .byWordWrapping
+        return label
+    }()
+    
+    let subtitleLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Congratulations on being wealthier than the following friends"
+        label.textColor = .gray
+        label.font = UIFont.systemFont(ofSize: 18)
         label.numberOfLines = 2
         label.lineBreakMode = .byWordWrapping
         return label
@@ -22,9 +32,11 @@ class HeaderView: UICollectionReusableView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(titleLabel)
+        addSubview(subtitleLabel)
         
-        addConstraintsWithFormat(format: "V:|[v0]|", views: titleLabel)
+        addConstraintsWithFormat(format: "V:|[v0][v1]|", views: titleLabel, subtitleLabel)
         addConstraintsWithFormat(format: "H:|-24-[v0]-36-|", views: titleLabel)
+        addConstraintsWithFormat(format: "H:|-24-[v0]-50-|", views: subtitleLabel)
     }
     
     required init?(coder aDecoder: NSCoder) {
