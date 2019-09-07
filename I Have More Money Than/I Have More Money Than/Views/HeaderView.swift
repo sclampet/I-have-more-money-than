a8876274b379d96ee8c91e17b23072a0ea9914b1
Scott Clampet <scottclampet@Scotts-MacBook-Pro-4.local>
2019-09-07 08:09:25 -0600

    create headerview file

---
+++ I Have More Money Than/I Have More Money Than/Views/HeaderView.swift
@@ -0,0 +1,22 @@
+//
+//  HeaderView.swift
+//  I Have More Money Than
+//
+//  Created by Scott Clampet on 9/7/19.
+//  Copyright © 2019 Scott Clampet. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UICollectionReusableView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .red
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
