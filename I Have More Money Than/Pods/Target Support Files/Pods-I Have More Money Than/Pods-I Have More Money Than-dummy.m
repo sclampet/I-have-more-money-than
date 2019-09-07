83213e5d67a8451510d30bd7c2b76a9af54e4913
Scott Clampet <scottclampet@Scotts-MacBook-Pro-4.local>
2019-09-07 11:42:20 -0600

    pod install SVProgressHUD

---
+++ I Have More Money Than/Pods/Target Support Files/Pods-I Have More Money Than/Pods-I Have More Money Than-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_I_Have_More_Money_Than : NSObject
+@end
+@implementation PodsDummy_Pods_I_Have_More_Money_Than
+@end
---
+++ I Have More Money Than/Pods/Target Support Files/Pods-I Have More Money ThanTests/Pods-I Have More Money ThanTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_I_Have_More_Money_ThanTests : NSObject
+@end
+@implementation PodsDummy_Pods_I_Have_More_Money_ThanTests
+@end
